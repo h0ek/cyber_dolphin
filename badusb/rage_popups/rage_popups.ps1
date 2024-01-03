6c8a22fb45f8970748a242eb9a70c3320b31a0b8
hoek <hoek@tuta.io>
2024-01-03 14:21:23 +0100

    Update rage_popups.ps1

--- badusb/rage_popups/rage_popups.ps1
+++ badusb/rage_popups/rage_popups.ps1
@@ -35,12 +35,12 @@ rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
 
 # Delete run box history
 
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+#reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
 
 # Delete powershell history
 
-Remove-Item (Get-PSreadlineOption).HistorySavePath
+#Remove-Item (Get-PSreadlineOption).HistorySavePath
 
 # Deletes contents of recycle bin
 
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+#Clear-RecycleBin -Force -ErrorAction SilentlyContinue
